1947865075d6859dda94f4a2880c25abffc7cd70
MeesVerbeeck <mees.verbeeck@student.ucll.be>
2025-03-13 09:34:51 +0100

    remove console log

--- Backend/controller/energyDataController.js
+++ Backend/controller/energyDataController.js
@@ -8,7 +8,6 @@ router.post('/', async (req, res) => {
         const energyData = await energyDataService.create(req.body);
         res.status(200).json({ message: 'Energy data created', energyData });
     } catch (error) {
-        console.log(error);
         res.status(400).json({ error: error.message });
     }
 });
