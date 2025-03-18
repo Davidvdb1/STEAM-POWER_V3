91f882e50f4a143cb17c7af883faaf16993fe5ab
HendrikGeurts <hendrik.geurts@student.ucll.be>
2025-03-18 10:57:02 +0100

    fixed energyData in prisma schema

--- Backend/controller/energyDataController.js
+++ Backend/controller/energyDataController.js
@@ -5,10 +5,12 @@ const router = express.Router();
 
 router.post('/', async (req, res) => {
     try {
+        console.log("body:" + res.body);
         const energyData = await energyDataService.create(req.body);
         res.status(200).json({ message: 'Energy data created', energyData });
     } catch (error) {
         res.status(400).json({ error: error.message });
+        console.log(error); 
     }
 });
 
--- Backend/service/energyDataService.js
+++ Backend/service/energyDataService.js
@@ -3,10 +3,15 @@ const EnergyData = require('../model/energyData');
 
 class EnergyDataService {
     async create(data) {
-        data.time = new Date(data.time);
+        console.log("Incoming data before processing:", data); // Debugging
+    
+        data.time = new Date(data.time); // Zorg ervoor dat het een DateTime is
+        console.log("Processed data:", data);
+    
         const energyData = new EnergyData(data);
         return await energyDataRepository.create(energyData);
     }
+    
 
     async getAllByGroup(groupId) {
         return await energyDataRepository.getAllByGroup(groupId);
