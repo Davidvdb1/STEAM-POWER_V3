55d3536a6a2c2eda135f49fe2d346aab67377dbf
HendrikGeurts <hendrik.geurts@student.ucll.be>
2025-04-22 10:02:20 +0200

    fix return on backend function and improved energy

--- Backend/service/energyDataService.js
+++ Backend/service/energyDataService.js
@@ -9,9 +9,9 @@ class EnergyDataService {
             data.time = new Date(data.time);
             const energyData = new EnergyData(data);
             const Energy = data.value /1024 * 3 * 0.5 * 2;
-            await energyDataRepository.create(energyData);
+            console.log(Energy);
             await groupRepository.addEnergyToGroup(data.groupId, Energy);
-            return;
+            return await energyDataRepository.create(energyData);
         } catch (error) {
             if (error instanceof utility.ValidationError) throw error;
             throw new utility.DatabaseError(`Error creating energy data: ${error.message}`);
