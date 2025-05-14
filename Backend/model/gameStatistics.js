7d81ccb15399dacc0b545b4bfc8ad0e63236ea6c
WardLooyens <113988769+WardLooyens@users.noreply.github.com>
2025-05-14 13:12:22 +0200

    Refactor GameStatistics model to improve validation logic and ensure proper instance checks

--- Backend/model/gameStatistics.js
+++ Backend/model/gameStatistics.js
@@ -4,12 +4,6 @@ const Checkpoint = require("./checkpoint");
 const Asset = require("./asset");
 
 class GameStatistics {
-<<<<<<< HEAD
-  constructor(
-    { id = undefined, currencies, building, groupID, checkpoint, assets },
-    validate = true
-  ) {
-=======
   constructor({
     id = undefined,
     currency,
@@ -18,7 +12,6 @@ class GameStatistics {
     checkpoints,
     assets,
   }, validate = true) {
->>>>>>> 3fd4af8e4e395454dcb70cb65ad5b625a6bcee71
     this.id = id;
     this.currency = currency;
     this.buildings = buildings;
@@ -29,40 +22,17 @@ class GameStatistics {
   }
 
   validate() {
-<<<<<<< HEAD
-    if (!(this.currencies instanceof Currency)) {
-      throw new Error("Invalid currencies (must be a Currency instance)");
-    }
-    if (
-      !Array.isArray(this.building) ||
-      !this.building.every((b) => b instanceof Building)
-    ) {
-      throw new Error(
-        "Invalid building (must be an array of Building instances)"
-      );
-=======
     if (!(this.currency instanceof Currency)) {
       throw new Error('Invalid currencies (must be a Currency instance)');
     }
     if (!Array.isArray(this.buildings) || !this.buildings.every(b => b instanceof Building)) {
       throw new Error('Invalid building (must be an array of Building instances)');
->>>>>>> 3fd4af8e4e395454dcb70cb65ad5b625a6bcee71
     }
     if (typeof this.groupID !== "string") {
       throw new Error("Invalid groupID (must be a string)");
     }
-<<<<<<< HEAD
-    if (
-      !Array.isArray(this.checkpoint) ||
-      !this.checkpoint.every((c) => c instanceof Checkpoint)
-    ) {
-      throw new Error(
-        "Invalid checkpoint (must be an array of Checkpoint instances)"
-      );
-=======
     if (!Array.isArray(this.checkpoints) || !this.checkpoints.every(c => c instanceof Checkpoint)) {
       throw new Error('Invalid checkpoint (must be an array of Checkpoint instances)');
->>>>>>> 3fd4af8e4e395454dcb70cb65ad5b625a6bcee71
     }
     if (
       !Array.isArray(this.assets) ||
