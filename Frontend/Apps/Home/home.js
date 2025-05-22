b22dd0e1bd607be7714bb1d4269965c3776cf13e
MeesVerbeeck <mees.verbeeck@student.ucll.be>
2025-05-22 12:06:48 +0200

    next change in comments

--- Frontend/Apps/Home/home.js
+++ Frontend/Apps/Home/home.js
@@ -2,6 +2,7 @@
 import "../../Components/navigation/tabHandler/tabHandler.js"
 import "../../Components/microbit/microbitBluetoothConnection/microbitBluetoothConnection.js"
 import "../../Components/microbit/microbitBasicBluetoothConnection/microbitBasicBluetoothConnection.js"
+import "../../Components/simulation/simulation.js"
 //#endregion IMPORTS
 
 //#region HOME
@@ -9,6 +10,7 @@ let template = document.createElement('template');
 template.innerHTML = /*html*/`
     <microbitbasicbluetoothconnection-れ></microbitbasicbluetoothconnection-れ>
     <tabhandler-れ></tabhandler-れ>
+    
 `;
 //#endregion HOME
 
@@ -19,6 +21,9 @@ window.customElements.define('home-ɮ', class extends HTMLElement {
         this._shadowRoot = this.attachShadow({ 'mode': 'open' });
         this._shadowRoot.appendChild(template.content.cloneNode(true));
         this.$example = this._shadowRoot.querySelector(".example");
+        // this.$simulation = this._shadowRoot.querySelector("simulation-れ");
+        // this.$simulation.setAttribute("hidden", true);
+        // this.$simulation.setAttribute("style", "display: none;");
     }
 
     // component attributes
@@ -30,9 +35,25 @@ window.customElements.define('home-ɮ', class extends HTMLElement {
 
     }
 
-    connectedCallback() {
+    // connectedCallback() {
+    //     console.log("connectedCallback home");
+    //     document.addEventListener("hideSimulation", this.hideSimulation.bind(this));
+    //     document.addEventListener("fetchSimulation", this.fetchSimulation.bind(this));
+    // }
 
-    }
+    // hideSimulation(event) {
+    //     console.log("hideSimulation home");
+    //     this.moveBefore(event.detail.node, null);
+    //     this.$simulation = this._shadowRoot.querySelector("simulation-れ");
+    //     this.$simulation.setAttribute("hidden", true);
+    //     this.$simulation.setAttribute("style", "display: none;");
+    // }
 
+    // fetchSimulation() {
+    //     console.log("fetchSimulation home");
+    //     const showSimulationEvent = new CustomEvent("showSimulation", { detail: { node: this.$simulation } });
+    //     console.log("sending showSimulationEvent");
+    //     document.dispatchEvent(showSimulationEvent);
+    // }
 });
 //#endregion CLASS
