5aa9c5b80d0f42cfafb71b760cadb2acc584e92a
TimVandeput <tim.vandeput@student.ucll.be>
2025-05-29 15:54:08 +0200

    Hier ga alles goed. Nu beginnen aan gamebuildings loaden bij laden van checkpoint

--- Backend/model/asset.js
+++ Backend/model/asset.js
@@ -1,52 +1,66 @@
+// asset.js
 class Asset {
   static allowedTypes = ['Windmolen', 'Waterrad', 'Zonnepaneel', 'Kerncentrale'];
 
-  constructor({ id = undefined, buildCost, destroyCost, energy, xLocation, yLocation, xSize, ySize, type }, validate = true) {
-    this.id = id;
-    this.buildCost = buildCost;
-    this.destroyCost = destroyCost;
-    this.energy = energy;
-    this.xLocation = xLocation;
-    this.yLocation = yLocation;
-    this.xSize = xSize;
-    this.ySize = ySize;
-    this.type = type;
+  constructor({
+    id = undefined,
+    buildCost,
+    destroyCost,
+    energy,
+    xLocation,
+    yLocation,
+    xSize,
+    ySize,
+    type,
+    gameStatisticsId = null,
+    checkpointId = null,
+  }, validate = true) {
+    this.id               = id;
+    this.buildCost        = buildCost;
+    this.destroyCost      = destroyCost;
+    this.energy           = energy;
+    this.xLocation        = xLocation;
+    this.yLocation        = yLocation;
+    this.xSize            = xSize;
+    this.ySize            = ySize;
+    this.type             = type;
+    this.gameStatisticsId = gameStatisticsId;
+    this.checkpointId     = checkpointId;
+
     if (validate) this.validate();
   }
 
   _validateFields() {
-    if (typeof this.buildCost !== 'number') throw new Error('Invalid buildCost');
+    if (typeof this.buildCost !== 'number')   throw new Error('Invalid buildCost');
     if (typeof this.destroyCost !== 'number') throw new Error('Invalid destroyCost');
-    if (typeof this.energy !== 'number') throw new Error('Invalid energy');
-    if (typeof this.xLocation !== 'number') throw new Error('Invalid xLocation');
-    if (typeof this.yLocation !== 'number') throw new Error('Invalid yLocation');
-    if (typeof this.xSize !== 'number') throw new Error('Invalid xSize');
-    if (typeof this.ySize !== 'number') throw new Error('Invalid ySize');
-    if (typeof this.type !== 'string') throw new Error('Invalid type');
+    if (typeof this.energy !== 'number')      throw new Error('Invalid energy');
+    if (typeof this.xLocation !== 'number')   throw new Error('Invalid xLocation');
+    if (typeof this.yLocation !== 'number')   throw new Error('Invalid yLocation');
+    if (typeof this.xSize !== 'number')       throw new Error('Invalid xSize');
+    if (typeof this.ySize !== 'number')       throw new Error('Invalid ySize');
+    if (typeof this.type !== 'string')        throw new Error('Invalid type');
+    if (!Asset.allowedTypes.includes(this.type)) {
+      throw new Error(`Invalid type: ${this.type}. Allowed: ${Asset.allowedTypes.join(', ')}`);
+    }
   }
 
   validate() {
     this._validateFields();
-    if (!Asset.allowedTypes.includes(this.type)) {
-      throw new Error(`Invalid type: ${this.type}. Must be one of: ${Asset.allowedTypes.join(', ')}`);
-    }
   }
 
   static from(prismaAsset) {
-    const Nature = require('./nature');
-    if (Nature.allowedTypes.includes(prismaAsset.type)) {
-      return Nature.from(prismaAsset);
-    }
     return new Asset({
-      id: prismaAsset.id,
-      buildCost: prismaAsset.buildCost,
-      destroyCost: prismaAsset.destroyCost,
-      energy: prismaAsset.energy,
-      xLocation: prismaAsset.xLocation,
-      yLocation: prismaAsset.yLocation,
-      xSize: prismaAsset.xSize,
-      ySize: prismaAsset.ySize,
-      type: prismaAsset.type
+      id:               prismaAsset.id,
+      buildCost:        prismaAsset.buildCost,
+      destroyCost:      prismaAsset.destroyCost,
+      energy:           prismaAsset.energy,
+      xLocation:        prismaAsset.xLocation,
+      yLocation:        prismaAsset.yLocation,
+      xSize:            prismaAsset.xSize,
+      ySize:            prismaAsset.ySize,
+      type:             prismaAsset.type,
+      gameStatisticsId: prismaAsset.gameStatisticsId,
+      checkpointId:     prismaAsset.checkpointId,
     });
   }
 }
--- Backend/model/gameBuildings.js
+++ Backend/model/gameBuildings.js
@@ -1,51 +1,41 @@
-const BuildingLevel = require("./buildingLevel");
-const Building = require("./building");
+// gameBuildings.js
+const Building      = require('./building');
+const BuildingLevel = require('./buildingLevel');
 
 class GameBuildings {
-  constructor(
-    { id = undefined, building, buildingLevel },
-    validate = true
-  ) {
-    this.id = id;
-    // this.gameStatistics = gameStatistics;
-    this.building = building;
-    this.buildingLevel = buildingLevel;
-    
+  constructor({
+    id = undefined,
+    gameStatisticsId = null,
+    checkpointId     = null,
+    building,
+    buildingLevel,
+  }, validate = true) {
+    this.id               = id;
+    this.gameStatisticsId = gameStatisticsId;
+    this.checkpointId     = checkpointId;
+    this.building         = building;
+    this.buildingLevel    = buildingLevel;
+
     if (validate) this.validate();
   }
 
   validate() {
-    // Check type without using instanceof for GameStatistics to avoid circular dependency
-    // if (this.gameStatistics && typeof this.gameStatistics !== 'object') {
-    //   throw new Error('Invalid gameStatistics (must be an object)');
-    // }
-    if (this.building && !(this.building instanceof Building)) {
+    if (!(this.building instanceof Building)) {
       throw new Error('Invalid building (must be Building)');
     }
-    if (this.buildingLevel && !(this.buildingLevel instanceof BuildingLevel)) {
+    if (!(this.buildingLevel instanceof BuildingLevel)) {
       throw new Error('Invalid buildingLevel (must be BuildingLevel)');
     }
   }
 
-  static from(prismaGameBuilding) {
-    // Skip validation during initial creation
-    const gameBuilding = new GameBuildings({
-      id: prismaGameBuilding.id,
-      // gameStatistics: null, // Don't convert GameStatistics to avoid circular dependency
-      building: null,
-      buildingLevel: null
-    }, false);
-
-    // Set properties after creation
-    if (prismaGameBuilding.building) {
-      gameBuilding.building = Building.from(prismaGameBuilding.building);
-    }
-    
-    if (prismaGameBuilding.buildingLevel) {
-      gameBuilding.buildingLevel = BuildingLevel.from(prismaGameBuilding.buildingLevel);
-    }
-    
-    return gameBuilding;
+  static from(prismaGB) {
+    return new GameBuildings({
+      id:               prismaGB.id,
+      gameStatisticsId: prismaGB.gameStatisticsId,
+      checkpointId:     prismaGB.checkpointId,
+      building:         prismaGB.building       ? Building.from(prismaGB.building)       : null,
+      buildingLevel:    prismaGB.buildingLevel  ? BuildingLevel.from(prismaGB.buildingLevel) : null,
+    });
   }
 }
 
