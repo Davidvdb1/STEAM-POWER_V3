7d220d77e305df9bbe2af34dd2110b7f66e1c20b
TimVandeput <tim.vandeput@student.ucll.be>
2025-05-30 12:54:22 +0200

    Fix logging statement in asset addition endpoint to use req.body

--- Backend/controller/gameStatisticsController.js
+++ Backend/controller/gameStatisticsController.js
@@ -134,7 +134,7 @@ router.post("/:id/currency/increment", async (req, res) => {
 // POST /gameStatistics/:id/assets
 // Adds an asset to a GameStatistics object
 router.post("/:id/assets", async (req, res) => {
-  console.log(res.body);
+  console.log(req.body);
   try {
     const asset = await gameStatisticsService.addAsset(req.params.id, req.body);
     res.status(201).json(asset);
