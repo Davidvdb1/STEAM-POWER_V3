a2c4077ec3b6b202c08a6f007830b0af28179b9d
TimVandeput <tim.vandeput@student.ucll.be>
2025-06-02 11:13:32 +0200

    Revert "added runsOnGreen to building model"
    This reverts commit a6f345d9bb320cf761847bacc4b94b89e3189be8.

--- Backend/model/building.js
+++ Backend/model/building.js
@@ -1,40 +1,31 @@
 /**
- * Represents a building entity with an identifier, name, and a flag indicating if it runs on green energy.
- *
- * @class
- * @property {string|undefined} id - The unique identifier of the building.
- * @property {string} name - The name of the building.
- * @property {boolean} runsOnGreen - Indicates whether the building runs on green energy.
+ * Represents a Building entity.
  *
  * @constructor
  * @param {Object} params - The parameters for the building.
- * @param {string|undefined} [params.id] - The unique identifier of the building.
+ * @param {string} [params.id] - The unique identifier of the building.
  * @param {string} params.name - The name of the building.
- * @param {boolean} params.runsOnGreen - Whether the building runs on green energy.
- * @param {boolean} [validate=true] - Whether to validate the building properties upon creation.
+ * @param {boolean} [validate=true] - Whether to validate the building on creation.
+ *
+ * @throws {Error} If validation fails.
  */
 class Building {
-  constructor({ id = undefined, name, runsOnGreen }, validate = true) {
+  constructor({ id = undefined, name }, validate = true) {
     this.id = id;
     this.name = name;
-    this.runsOnGreen = runsOnGreen;
 
     if (validate) this.validate();
   }
+
   /**
-   * Validates the properties of the building instance.
-   * Throws an error if the 'name' property is not a string
-   * or if the 'runsOnGreen' property is not a boolean.
+   * Validates the building’s properties.
    *
-   * @throws {Error} If 'name' is not a string or 'runsOnGreen' is not a boolean.
+   * @throws {Error} If `name` is not a string.
    * @returns {void}
    */
   validate() {
     if (typeof this.name !== "string") {
       throw new Error("Invalid name");
-    }
-    if (typeof this.runsOnGreen !== "boolean") {
-      throw new Error("Invalid runsOnGreen");
     }
   }
 
@@ -45,7 +36,6 @@ class Building {
    * @param {Object} prismaBuilding - The Prisma building object.
    * @param {string} prismaBuilding.id - The unique identifier from Prisma.
    * @param {string} prismaBuilding.name - The name from Prisma.
-   * @param {boolean} [prismaBuilding.runsOnGreen=false] - Whether the building runs on green energy.
    * @returns {Building} The created Building instance.
    */
   static from(prismaBuilding) {
